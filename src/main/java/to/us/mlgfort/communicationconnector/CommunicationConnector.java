0c1787f6526fe7d5b38e49a96ade7d4b233d2fe0
RoboMWM <robomwm@users.noreply.github.com>
2017-07-31 12:58:12 -0700

    whoops lol

--- src/main/java/to/us/mlgfort/communicationconnector/CommunicationConnector.java
+++ src/main/java/to/us/mlgfort/communicationconnector/CommunicationConnector.java
@@ -6,17 +6,11 @@ import com.cnaude.purpleirc.PurpleIRC;
 import com.teej107.slack.MessageSentFromSlackEvent;
 import com.teej107.slack.Slack;
 import github.scarsz.discordsrv.DiscordSRV;
-import github.scarsz.discordsrv.api.events.DiscordGuildMessageReceivedEvent;
 import github.scarsz.discordsrv.util.DiscordUtil;
 import org.apache.commons.lang.WordUtils;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -29,17 +23,15 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class CommunicationConnector extends JavaPlugin implements Listener
 {
     private final String SERBUR_NAME = "Da_Serbur_Sez";
-    ConsoleCommandSender SERBUR_SENDER;
     Slack slack;
     PurpleIRC purpleIRC;
     public void onEnable()
     {
-        SERBUR_SENDER = getServer().getConsoleSender();
         PluginManager pm = getServer().getPluginManager();
         getServer().getPluginManager().registerEvents(this, this);
         slack = (Slack)pm.getPlugin("SlackIntegration");
         purpleIRC = (PurpleIRC)pm.getPlugin("PurpleIRC");
-        DiscordSRV.api.subscribe(new CommunicationConnector());
+        DiscordSRV.api.subscribe(new DumDiscord(this));
     }
 
     private void sendToIRC(final String message)
@@ -83,8 +75,8 @@ public class CommunicationConnector extends JavaPlugin implements Listener
 
     //MC Listeners//
     //I might be better off not attempting to implement this since literally every plugin implements this anyways...
-    @Deprecated
-    private void sendToApps(CommandSender sender, String message)
+    //@Deprecated
+    //private void sendToApps(CommandSender sender, String message)
     {
 //        String name = sender.getName();
 //        boolean avatar = true;
@@ -99,31 +91,31 @@ public class CommunicationConnector extends JavaPlugin implements Listener
         //sendToIRC(name + ": " + message);
     }
 
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    private void onJoin(PlayerJoinEvent event)
-    {
-        String joinMessage = event.getJoinMessage();
-        if (joinMessage == null || joinMessage.isEmpty())
-            joinMessage = event.getPlayer().getName() + " IZ BAK 4 MOAR MEINKRAFT!!!!!!1111111111!!!1";
-        sendToApps(SERBUR_SENDER, joinMessage);
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    private void onQuit(PlayerQuitEvent event)
-    {
-        String quitMessage = event.getQuitMessage();
-        if (quitMessage == null || quitMessage.isEmpty())
-            quitMessage = event.getPlayer().getName() + " left us in loneliness :c";
-        sendToApps(SERBUR_SENDER, quitMessage);
-    }
+//    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+//    private void onJoin(PlayerJoinEvent event)
+//    {
+//        String joinMessage = event.getJoinMessage();
+//        if (joinMessage == null || joinMessage.isEmpty())
+//            joinMessage = event.getPlayer().getName() + " IZ BAK 4 MOAR MEINKRAFT!!!!!!1111111111!!!1";
+//        sendToApps(SERBUR_SENDER, joinMessage);
+//    }
+//
+//    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+//    private void onQuit(PlayerQuitEvent event)
+//    {
+//        String quitMessage = event.getQuitMessage();
+//        if (quitMessage == null || quitMessage.isEmpty())
+//            quitMessage = event.getPlayer().getName() + " left us in loneliness :c";
+//        sendToApps(SERBUR_SENDER, quitMessage);
+//    }
 
     //TODO: deathmessages deathmessage messagedeath ded
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    private void onAsyncChat(AsyncPlayerChatEvent event)
-    {
-        sendToApps(event.getPlayer(), event.getMessage());
-    }
+//    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+//    private void onAsyncChat(AsyncPlayerChatEvent event)
+//    {
+//        sendToApps(event.getPlayer(), event.getMessage());
+//    }
 
     //App Listeners//
 
