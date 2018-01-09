527b4cfe21944a552094613d3e288d3686bac428
RoboMWM <robomwm@users.noreply.github.com>
2018-01-08 16:31:23 -0800

    append attached junk in dumcord

--- src/main/java/to/us/mlgfort/communicationconnector/DumCord.java
+++ src/main/java/to/us/mlgfort/communicationconnector/DumCord.java
@@ -2,6 +2,7 @@ package to.us.mlgfort.communicationconnector;
 
 import github.scarsz.discordsrv.api.Subscribe;
 import github.scarsz.discordsrv.api.events.DiscordGuildMessageReceivedEvent;
+import github.scarsz.discordsrv.dependencies.jda.core.entities.Message;
 
 /**
  * Created on 7/31/2017.
@@ -22,5 +23,7 @@ public class DumCord
     public void onDiscordMessageReceived(DiscordGuildMessageReceivedEvent event)
     {
         instance.sendToApps(Apps.DUMCORD, event.getMember().getEffectiveName(), event.getMessage().getContent());
+        for (Message.Attachment attachment : event.getMessage().getAttachments())
+            instance.sendToApps(Apps.DUMCORD, event.getMember().getEffectiveName(), attachment.getUrl());
     }
 }
